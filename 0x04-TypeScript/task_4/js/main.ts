dc61b6942aaca988c3a51b99743eb25783b7171f
juskins <omojuwababatunde1@gmail.com>
2025-11-09 10:30:45 +0100

    Add task 10

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,6 +1,8 @@
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+const { Cpp, Java, React } = Subjects;
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
 export const cTeacher: Subjects.Teacher = {
 	firstName: 'John',
